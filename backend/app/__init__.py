b14844b13e48269d400a96423a4dd9e73d3139a6
Gustavo <gufreschi.ads@gmail.com>
2025-05-15 21:44:45 -0300

    arquivos criados seguindo a estrutura necessaria, inicio da api para receber e salvar imagens

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = 'uploads'
 
-    from .routes import bp as routes_bp
-    app.register_blueprint(routes_bp)
+    from .routes import image_bp
+    app.register_blueprint(image_bp)
 
     return app
